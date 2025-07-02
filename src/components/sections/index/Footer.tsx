3725d2fae834c94228f0083ed985f157c9e47314
RAJ <135547952+bibhanshuraj19@users.noreply.github.com>
2025-07-02 21:33:25 +0530

    Initial commit

---
+++ src/components/sections/index/Footer.tsx
@@ -0,0 +1,14 @@
+import Divider from "@/components/Divider";
+
+export default function Footer() {
+    return (
+        <>
+            <footer className="max-w-4xl w-full flex flex-col mx-auto pt-4 pb-2">
+                <Divider />
+                <p className="text-center font-semibold text-base pt-2">
+                    Made with NextJS, TailwindCSS.
+                </p>
+            </footer>
+        </>
+    );
+}
