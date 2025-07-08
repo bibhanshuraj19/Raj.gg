4af19efaad0b00bee02a838387621a42eaf0edc0
RAJ <135547952+bibhanshuraj19@users.noreply.github.com>
2025-07-08 14:44:26 +0530

    favicon ico

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -17,12 +17,6 @@ export default function App({ Component, pageProps }: AppProps) {
           url: `https://aiden.gg${router.asPath.split("?")[0] === "/" ? "" : router.asPath.split("?")[0]}`,
           title: "Raj.gg",
           description: "Raj's personal site",
-          images: [
-            {
-              url: "https://r2.e-z.host/2082d908-7c65-4fc3-b02a-5f50f9141543/lbo1x6wn.png",
-              alt: "Oliver",
-            },
-          ],
         }}
       />
       <Component {...pageProps} />
