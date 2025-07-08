ddbb1efcfad3330d89c00a84de588032464724f9
RAJ <135547952+bibhanshuraj19@users.noreply.github.com>
2025-07-08 14:52:56 +0530

    favicon ico

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,4 @@
 import "@/styles/globals.css";
-import { NextSeo } from "next-seo";
 import type { AppProps } from "next/app";
 import { useRouter } from "next/router";
 
@@ -8,17 +7,6 @@ export default function App({ Component, pageProps }: AppProps) {
 
   return (
     <>
-      <NextSeo
-        title={"Raj.gg"}
-        description={"Raj's personal site"}
-        canonical={`https://aiden.gg${router.asPath.split("?")[0] === "/" ? "" : router.asPath.split("?")[0]}`}
-        themeColor={"#2563eb"}
-        openGraph={{
-          url: `https://aiden.gg${router.asPath.split("?")[0] === "/" ? "" : router.asPath.split("?")[0]}`,
-          title: "Raj.gg",
-          description: "Raj's personal site",
-        }}
-      />
       <Component {...pageProps} />
     </>
   );
