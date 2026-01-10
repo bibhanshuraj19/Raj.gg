95c246be70af00cd654ff0fe7ade026b9fb84813
Raj <bibhanshuraj19@gmail.com>
2026-01-10 15:32:17 +0530

    updated hero

--- src/components/sections/index/Projects.tsx
+++ src/components/sections/index/Projects.tsx
@@ -16,7 +16,7 @@ export default function Projects() {
           transition={{ duration: 0.5, delay: 0.1, ease: [0.39, 0.21, 0.12, 0.96] }}
           viewport={{ amount: 0.1, once: true }}
         >
-          Other Projects
+          Projects
         </motion.h1>
         <ul className={`${showAll ? '' : 'max-h-[100rem]'} grid md:grid-cols-2 pt-6 pb-1 grid-cols-1 gap-4 overflow-hidden`}>
           {/* Add new <ProjectCard /> components here */}
